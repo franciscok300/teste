4ece09095f45e1710a6df82d517abe39ca16a955
franciscok300 <44905381+franciscok300@users.noreply.github.com>
2020-05-27 21:42:39 -0300

    teste
    teste 2 aula

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return(
+      <>
+    <Routes />
+            <GlobalStyle/>
+        </>
+  );
+
+
+}
+
+export default App;
